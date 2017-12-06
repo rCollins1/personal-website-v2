049127b7826407c0d4bf57cb597e3b5f103b1ca9
Rachelyn Collins <rachelyn.collins@gmail.com>
2017-12-06 13:34:34 -0500

    Added some questions

--- Chatbot testing/chatbot.js
+++ Chatbot testing/chatbot.js
@@ -1,7 +1,33 @@
 $(document).ready(function(){
 
-	$("#o-1a").click(function(){
-			$("#o-1b").hide();
-		});
+	var o1, o2, o3, o4, o5, o6 = "";
 
-});	
+	$("#o-1a").click(function(){var o1 = a;});
+
+	$("#o-1b").click(function(){var o1 = b;});
+
+	$("#o-2a").click(function(){var o2 = a;});
+
+	$("#o-1a").click(function(){var o2 = b;});
+
+	$("#o-3a").click(function(){var o3 = a;});
+
+	$("#o-3b").click(function(){var o3 = b;});
+
+	$("#o-4a").click(function(){var o4 = a;});
+
+	$("#o-4b").click(function(){var o4 = b;});
+
+	$("#o-4c").click(function(){var o4 = c;});
+
+	$("#o-5a").click(function(){var o5 = a;});
+
+	$("#o-5b").click(function(){var o5 = b;});
+
+	$("#o-5c").click(function(){var o5 = c;});
+
+	$("#o-6a").click(function(){var o5 = a;});
+
+	$("#o-6b").click(function(){var o5 = b;});
+
+});
