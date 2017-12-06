5ee9c1c9be8fb3767aa25dd013f35b15b1b172ff
Rachelyn Collins <rachelyn.collins@gmail.com>
2017-12-06 16:20:01 -0500

    More dialog added + show/hide

--- Chatbot testing/chatbot.js
+++ Chatbot testing/chatbot.js
@@ -1,33 +1,71 @@
 $(document).ready(function(){
 
 	var o1, o2, o3, o4, o5, o6 = "";
+	var study, coop = 0;
 
-	$("#o-1a").click(function(){var o1 = a;});
+	$("#o-1a").click(function(){
+		o1 = "a"; console.log(o1);
+		study = 1; console.log("study: " + study);
+		$("#u-1a, #b-2a, #b-2b, #b-2c, #b-2d").removeClass("hide");
+		$("#o-1a, #o-1b").addClass("hide");
+		if (coop != 1) {
+			$("#o-2a").removeClass("hide");
+		}
+		$("#o-2c").removeClass("hide");
+		$("#o-2b").removeClass("hide");
+	});
 
-	$("#o-1b").click(function(){var o1 = b;});
+ 	$("#o-1b").click(function(){
+ 		o1 = "b"; console.log(o1);
+ 		coop = 1; console.log("coop: " + coop);
+ 	});
 
-	$("#o-2a").click(function(){var o2 = a;});
+ 	$("#o-2a").click(function(){
+ 		o2 = "a"; console.log(o2);
+ 	});
 
-	$("#o-1a").click(function(){var o2 = b;});
+ 	$("#o-2b").click(function(){
+ 		o2 = "b"; console.log(o2);
+ 	});
 
-	$("#o-3a").click(function(){var o3 = a;});
+ 	$("#o-3a").click(function(){
+ 		o3 = "a"; console.log(o3);
+ 	});
 
-	$("#o-3b").click(function(){var o3 = b;});
+ 	$("#o-3b").click(function(){
+ 		o3 = "b"; console.log(o3);
+ 	});
 
-	$("#o-4a").click(function(){var o4 = a;});
+ 	$("#o-4a").click(function(){
+ 		o4 = "a"; console.log(o4);
+ 	});
 
-	$("#o-4b").click(function(){var o4 = b;});
+ 	$("#o-4b").click(function(){
+ 		o4 = "b"; console.log(o4);
+ 	});
 
-	$("#o-4c").click(function(){var o4 = c;});
+ 	$("#o-4c").click(function(){
+ 		o4 = "c"; console.log(o4);
+ 	});
 
-	$("#o-5a").click(function(){var o5 = a;});
+ 	$("#o-5a").click(function(){
+ 		o5 = "a"; console.log(o5);
+ 	});
 
-	$("#o-5b").click(function(){var o5 = b;});
+ 	$("#o-5b").click(function(){
+ 		o5 = "b"; console.log(o5);
+ 	});
 
-	$("#o-5c").click(function(){var o5 = c;});
+ 	$("#o-5c").click(function(){
+ 		o5 = "c"; console.log(o5);
+ 	});
 
-	$("#o-6a").click(function(){var o5 = a;});
+ 	$("#o-6a").click(function(){
+ 		o6 = "a"; console.log(o6);
+ 	});
 
-	$("#o-6b").click(function(){var o5 = b;});
+ 	$("#o-6b").click(function(){
+ 		o6 = "b"; console.log(o6);
+ 	});
 
 });
